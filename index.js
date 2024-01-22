f4945cbe45aaa2939a7d6895a8b97ef7187fbe8c
Favour Afolayan <favourafolayan@gmail.com>
2024-01-22 13:33:47 +0100

    Complete the footer design

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const faqItems = document.querySelectorAll('.faq');
+const footerLinksContainer = document.querySelector('.footer-links-container');
 
 function expand(event) {
   const faqItem = event.currentTarget;
@@ -11,3 +12,35 @@ function expand(event) {
 }
 
 faqItems.forEach((faqItem) => faqItem.addEventListener('click', expand));
+
+const footerLinks = [
+  {
+    title: 'Product',
+    links: ['Overview', 'Features', 'Solutions', 'Tutorials', 'Pricing'],
+  },
+  {
+    title: 'Company',
+    links: ['About us', 'Careers', 'Press', 'News', 'Contact'],
+  },
+  {
+    title: 'Resources',
+    links: ['Blog', 'Events', 'Help centre', 'Tutorials', 'Support'],
+  },
+  {
+    title: 'Legal',
+    links: ['Terms', 'Privacy', 'Cookies', 'Licenses', 'Contact'],
+  },
+];
+
+footerLinksContainer.innerHTML = footerLinks
+  .map(
+    (footerLink) => `<div class='footer-links flex column'>
+<h1 class='links-title'>${footerLink.title}</h1>
+<ul class='links-body flex column'>
+${footerLink.links
+  .map((link) => `<li class='link'><a href="#">${link}</a></li>`)
+  .join('')}
+</ul>
+</div>`
+  )
+  .join('');
